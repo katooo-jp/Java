6724896153e14d5d3deccf623555fc68d5f12629
Kato Yusuke <katooo.py@gmail.com>
2021-10-22 13:34:42 +0900

    JavaBasic(06)

--- Study/Basics/Basic06.java
+++ Study/Basics/Basic06.java
@@ -8,6 +8,10 @@ class Human {
   private int age;
   private int height;
   private int weight;
+  private int myNumber;
+  // static付きのフィールドは宣言されたクラスの全インスタンスで共有されるクラス変数となる
+  // カウンターなどに使うと便利
+  private static int counter = 0;
 
   // コンストラクタ
   Human(String name, int age, int height, int weight) {
@@ -15,9 +19,14 @@ class Human {
     this.age = age;
     this.height = height;
     this.weight = weight;
+    this.myNumber = ++counter;
   }
 
-  // メソッド
+  // インスタンスメソッド　staticなしでインスタンスに対して起動できるメソッド。
+  // static付きであればクラスメソッドとなりインスタンスに関係なく使える
+  // アクセッサ　ゲッタとセッタ
+
+  // ゲッタ　フィールドの値を取得するメソッド
   String getName() {
     return this.name;
   }
@@ -33,6 +42,24 @@ class Human {
   int getWeight() {
     return this.weight;
   }
+
+  int getMyNumber() {
+    return this.myNumber;
+  }
+
+  // セッタ　フィールドに値をセットするメソッド
+  void setAge(int age) {
+    this.age = age;
+  }
+
+  void setHeight(int height) {
+    this.height = height;
+  }
+
+  void setWeight(int weight) {
+    this.weight = weight;
+  }
+
 }
 
 
@@ -45,6 +72,12 @@ public class Basic06 {
     };
     // メソッドの使い方
     System.out.println(humans[0].getName() + "さんの体重は" + humans[0].getWeight() + "kgです。");
+    System.out.println(humans[0].getName() + "さんのマイナンバーは" + humans[0].getMyNumber() + "です。");
+
     System.out.println(humans[1].getName() + "さんの体重は" + humans[1].getWeight() + "kgです。");
+    System.out.println(humans[1].getName() + "さんのマイナンバーは" + humans[1].getMyNumber() + "です。");
+
+    humans[1].setWeight(70);
+    System.out.println(humans[1].getName() + "さんの体重は変わって" + humans[1].getWeight() + "kgになりました。");
   }
 }
